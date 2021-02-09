bb6186759e4ff8fa9da56519d1dd353d0e54b032
MechanicalWood <17795837526@163.com>
2021-02-09 13:09:03 +0800

    git push test

---
+++ src/com/mechanicalwood/Test.java
@@ -0,0 +1,11 @@
+package com.mechanicalwood;
+
+/**
+ * @Auther MechanicalWood
+ * @Date 2021/2/9
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
